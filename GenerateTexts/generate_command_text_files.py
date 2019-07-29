7ae530e4f182c150862377fe6ecfdea1ff2517d6
SamuelStentz <samuelstentz@gmail.com>
2019-07-29 11:39:44 -0400

    fixed early stopping to meet significantly better stopping criteria

--- GenerateTexts/generate_command_text_files.py
+++ GenerateTexts/generate_command_text_files.py
@@ -27,7 +27,8 @@ with open("graph_generation.txt", "w") as fh:
         
 # possible parameters for model
 learning_rate = [.005, .01]
-epochs = [400, 600]
+epochs = [1000]
+early_stopping = [200]
 graph_conv = [20, 10]
 num_conv = [1, 2, 3]
 connected = [20]
@@ -41,36 +42,39 @@ accuracy_test = False
 accuracy_validation = True
 
 model_options = [learning_rate, epochs, graph_conv, num_conv, connected, num_connected,
-                 dropout, attention_dim, attention_bias, model, max_degree]
+                 dropout, attention_dim, attention_bias, model, max_degree, early_stopping]
 
 model_options_combos = list(itertools.product(*model_options))
-train_command_template = "python3 train.py -learning_rate {0} -epochs {1} -early_stopping {1} -graph_conv_dimensions {2} -dropout {3} "\
+train_command_template = "python3 train.py -learning_rate {0} -epochs {1} -graph_conv_dimensions {2} -dropout {3} "\
                             "-attention_dim {4} -attention_bias {5} -model {6} -max_degree {7} -connected_dimensions {8} -dataset {9} "\
-                            "-save_test {10} -save_validation {11}"
+                            "-save_test {10} -save_validation {11} -early_stopping {12}"
 
 graph_options = [params, ratios, selectors]
 graph_options_combos = list(itertools.product(*graph_options))
 
+
+
 # preliminary models
 with open("model_generation.txt", "w") as fh:
     for graph_option in graph_options_combos:
+        # all datasets to search through
+        dataset_ls = []
+        p = "["
+        for pr in proteins:
+            p+="protease_{3}_selector_{2}_ratio_{1}_params_{0},".format(graph_option[0], graph_option[1], graph_option[2], pr)
+        p += "]"
+        dataset_ls.append(p)
+        dataset_ls = dataset_ls + ["protease_{3}_selector_{2}_ratio_{1}_params_{0}".format(graph_option[0], graph_option[1], graph_option[2], pr) for pr in proteins]
         for model_option in model_options_combos:
-            # name of the graph from first slurm batch command
-            p = "["
-            for pr in proteins:
-                p+="protease_{3}_selector_{2}_ratio_{1}_params_{0},".format(graph_option[0], graph_option[1], graph_option[2], pr)
-            p += "]"
-            graph_name = p
-            
-            gcd = "["
-            for x in range(model_option[3]): gcd += str(model_option[2])+","
-            gcd += "]"
-            cd = "["
-            for x in range(model_option[5]): cd += str(model_option[4])+","
-            cd += "]"
-            
-            train_command = train_command_template.format(model_option[0], model_option[1], gcd, model_option[6],
-                                                         model_option[7], model_option[8], model_option[9],
-                                                         model_option[10], cd, graph_name,
-                                                         accuracy_test, accuracy_validation)
-            fh.write(train_command + "\n")
+            for dataset in dataset_ls:
+                gcd = "["
+                for x in range(model_option[3]): gcd += str(model_option[2])+","
+                gcd += "]"
+                cd = "["
+                for x in range(model_option[5]): cd += str(model_option[4])+","
+                cd += "]"
+                train_command = train_command_template.format(model_option[0], model_option[1], gcd, model_option[6],
+                                                             model_option[7], model_option[8], model_option[9],
+                                                             model_option[10], cd, dataset,
+                                                             accuracy_test, accuracy_validation, model_option[11])
+                fh.write(train_command + "\n")
